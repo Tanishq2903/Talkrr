694629426ea4d5963d2f56e705b85f9e808bdca4
Tanishq2903 <maharshitanishq@gmail.com>
2024-07-04 18:31:02 +0530

    Redux and Password page

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
-import router from './routes';
-// import { Provider } from 'react-redux'
-// import { store } from './redux/store';
+import router from './rotues';
+import { Provider } from 'react-redux'
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
       <RouterProvider router={router}>
         <App />
       </RouterProvider>
+    </Provider>
   </React.StrictMode>
 );
 
